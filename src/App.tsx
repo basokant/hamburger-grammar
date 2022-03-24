81590fac21c522b2460c8b9719b70f67efdb824a
basokant <basokanthan@gmail.com>
2022-03-23 22:46:02 -0400

    add header links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import logo from './assets/logo512.png';
-import modalButton from './assets/infoButton.png';
+import modalButton from './assets/infobutton.png';
 import TextInput from './components/TextInput';
 import Burger from './components/Burger';
 import './App.css';
@@ -21,8 +21,8 @@ function App() {
         <Burger input={input} />
       </div>
       <div className="App-footer">
-        <p>Made by Ben Asokanthan.</p>
-        <p>inspired by a meme.</p>
+        <p>Made by <a target="_blank" href="https://github.com/basokant">Ben Asokanthan.</a></p>
+        <p>Inspired by a <a target="_blank" href="https://knowyourmeme.com/photos/1839735-oreo-grammar">meme</a>.</p>
       </div>
     </div>
   );
