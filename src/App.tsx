781b422f2663adcd04ddd58521d8171718b0de30
basokant <basokanthan@gmail.com>
2022-03-24 12:00:56 -0400

    finish modal styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import logo from './assets/logo512.png';
-import modalButton from './assets/infobutton.png';
 import TextInput from './components/TextInput';
 import Burger from './components/Burger';
 import './App.css';
+
+import modalButton from './assets/infobutton.png';
+import closeButton from './assets/closebutton.png';
+import topbun from './assets/topbun.png';
+import lettuce from './assets/lettuce.png';
+import tomato from './assets/tomato.png';
+import cheese from './assets/cheese.png';
+import meat from './assets/meat.png';
+import bottombun from './assets/bottombun.png';
 
 const url = new URL(window.location.href);
 let params = new URLSearchParams(url.search);
@@ -31,15 +39,60 @@ function App() {
     setIsOpen(false);
   }
 
+  const modalStyles = {
+    content: {
+      backgroundColor: "#FFC46D",
+      border: "none",
+      fontFamily: "Montserrat",
+      fontSize: "1.5em",
+      borderRadius: "80px",
+    },
+  }
+
   return (
     <div className="App">
       <Modal
+        closeTimeoutMS={300}
         isOpen={isModalOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
+        style={modalStyles}
         contentLabel="Hello World"
       >
-        Hello World
+        <img src={closeButton} className="modal-button close" onClick={closeModal}/>
+        <p className="modal-title">Ingredients</p>
+        <div className="flex-grid">
+        <div className="col">
+            <p>ham</p>
+            <img className="ingredient" src={topbun} alt="" />
+          </div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">
+            <p>b</p>
+            <img className="ingredient" src={lettuce} alt="" />
+          </div>
+          <div className="col">
+            <p>u</p>
+            <img className="ingredient" src={tomato} alt="" />
+          </div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">
+            <p>r</p>
+            <img className="ingredient" src={cheese} alt="" />
+          </div>
+          <div className="col">
+            <p>g</p>
+            <img className="ingredient" src={meat} alt="" />
+          </div>
+        </div>
+        <div className="flex-grid">
+        <div className="col">
+            <p>er</p>
+            <img className="ingredient" src={bottombun} alt="" />
+          </div>
+        </div>
       </Modal>
       <div className="App-header">
         <img src={modalButton} className="modal-button" onClick={openModal}/>
